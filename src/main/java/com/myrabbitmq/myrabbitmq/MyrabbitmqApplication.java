07e1c810a94e4bf241bc6bc7172c2902e6e29d4f
jtq <jtq@winning.com.cn>
2020-06-29 13:13:29 +0800

    Initial commit

---
+++ src/main/java/com/myrabbitmq/myrabbitmq/MyrabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.myrabbitmq.myrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyrabbitmqApplication.class, args);
+    }
+
+}
